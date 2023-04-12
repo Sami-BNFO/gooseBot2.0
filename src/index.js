d3833c8ac21b3ee28b71699ade24aa171a9f6d61
CanadianDoc <78990604+CanadianDoc@users.noreply.github.com>
2023-04-12 10:47:03 -0600

    index.js

---
+++ src/index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+const {
+	Client,
+	Collection,
+	GatewayIntentBits,
+	Activity,
+	ActivityType,
+} = require("discord.js");
+const fs = require("fs");
+const bot = new Client({ intents: GateWayIntentBits.Guilds });
+bot.commands = new Collection();
+bot.buttons = new Collection();
+bot.commandArray = [];
+
+const functionFolders = fs.readdirSync(`./src/functions`);
+for (const folder of functionFolders) {
+	const functionFiles = fs
+		.readdirSync(`./src/functions/${folder}`)
+		.filter((file) => file.endsWith(".js"));
+	for (const file of functionFiles) {
+		require(`./functions/${folder}/${file}`)(bot);
+	}
+}
+
+bot.eventHandler();
+bot.commandHandler();
+bot.login(process.env.token);
