37ace0f31f68453e670dca622dd7be42342d851c
SamiMAnsari1 <130588384+SamiMAnsari1@users.noreply.github.com>
2023-04-13 16:56:15 +0100

    Update ban.js
    working

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -1,56 +1,120 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const {
+	SlashCommandBuilder,
+	EmbedBuilder,
+	PermissionFlagsBits,
+} = require("discord.js");
 
+//this creates the /kick command interface
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("ban")
-    .setDescription("Ban a member")
-    .addUserOption((option) =>
-      option
-        .setName("target")
-        .setDescription("The member to ban")
-        .setRequired(true)
-    )
-    .addStringOption((option) =>
-      option
-        .setName("reason")
-        .setDescription("The reason for banning the member")
-        .setRequired(false)
-    )
-    .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+	data: new SlashCommandBuilder()
+		.setName("ban")
+		.setDescription(
+			"Permanently bans a user from the guild"
+		)
+		.addUserOption(
+			(option) =>
+				option
+					.setName("name")
+					.setDescription("Who do you want to ban")
+					.setRequired(true) //
+		)
+		.addStringOption((option) =>
+			option
+				.setName("reason")
+				.setDescription("Reason:")
+				.setRequired(false)
+		)
+		.setDefaultMemberPermissions(
+			PermissionFlagsBits.Administrator
+		),
 
-  async execute(interaction, bot) {
-    const target = interaction.options.getUser("target");
-    const reason =
-      interaction.options.getString("reason") || "No reason provided";
-
-    if (target.id === bot.user.id) {
-      return interaction.reply({
-        content: "You can't ban me!",
-        ephemeral: true,
-      });
-    }
-
-    if (target.id === interaction.user.id) {
-      return interaction.reply({
-        content: "You can't ban yourself!",
-        ephemeral: true,
-      });
-    }
-    const member = interaction.guild.members.cache.get(target.id);
-    if (!member.bannable) {
-      return interaction.reply({
-        content: "I can't ban that member!",
-        ephemeral: true,
-      });
-    }
-
-    await member.ban({
-      deleteMessageSeconds: 60 * 60 * 24 * 7,
-      reason: reason,
-    });
-    interaction.reply({
-      content: `Banned ${target.tag}!`,
-      ephemeral: true,
-    });
-  },
+	async execute(interaction, bot) {
+		const user = interaction.options.getUser("name");
+		const member = interaction.guild.members.cache.get(
+			user.id
+		);
+		const reason =
+			interaction.options.getString("reason") ||
+			"No Reason Given";
+		const footers = [
+			"What a nerd!",
+			"Imagine getting banned!",
+			"Skill Issue",
+			`Don't be like ${user.tag}!`,
+			"💀",
+		];
+		const ranOption = Math.floor(
+			Math.random() * footers.length
+		);
+		const banEmbed = new EmbedBuilder()
+			.setTitle(`${user.tag} Has been banned!`)
+			.setAuthor({
+				name: "Goose",
+				iconURL:
+					"https://cdn-icons-png.flaticon.com/512/2826/2826187.png",
+			})
+			.setThumbnail(
+				"https://i.pinimg.com/originals/08/41/81/084181642c396ecaa191e085b73d5d61.png"
+			)
+			.addFields(
+				{
+					name: "Given Reason:\n",
+					value: `${reason}`,
+					inline: true,
+				},
+				{
+					name: "Banned by:\n",
+					value: `${interaction.user.tag}`,
+					inline: true,
+				}
+			)
+			.setTimestamp(Date.now())
+			.setFooter({ text: ` \n${footers[ranOption]}` })
+			.setColor("DarkRed");
+		if (user.id === interaction.user.id) {
+			return interaction.reply({
+				content: "Why",
+				ephemeral: true,
+			});
+		}
+		if (user.id === "1002697963375702070") {
+			return interaction.reply({
+				content: "CANT BAN SAMI LL",
+				ephemeral: true,
+			});
+		}
+		if (user.id === bot.user.id) {
+			return interaction.reply({
+				content:
+					"Goose feels betrayed, sleep with one eye open tonight.",
+				ephemeral: true,
+			});
+		}
+		if (!member.bannable) {
+			console.log(member);
+			return interaction.reply({
+				content: "User cannot be banned",
+				ephemeral: true,
+			});
+		} else {
+			try {
+				await member.ban({
+					deleteMessageSeconds: 60 * 60 * 24 * 7,
+					reason: reason,
+				});
+				return interaction.reply({
+					embeds: [banEmbed],
+				});
+			} catch (err) {
+				console.error(err);
+			}
+		}
+	},
 };
+
+/*const embed = new EmbedBuilder()
+            .setTitle("User Kicked!")
+            .setDescription(`Reason:\n${reason}`)
+            .setTimestamp(Date.now())
+            .setColor(0xe3c05f);
+        */
