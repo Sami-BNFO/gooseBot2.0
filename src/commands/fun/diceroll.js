52f9c35815a4facd663827e980fb80a3f41efb92
SamiMAnsari1 <130588384+SamiMAnsari1@users.noreply.github.com>
2023-04-13 17:07:11 +0100

    Create diceroll.js
    re added

---
+++ src/commands/fun/diceroll.js
@@ -0,0 +1,66 @@
+const {
+	SlashCommandBuilder,
+	EmbedBuilder,
+	Embed,
+	Message,
+} = require("discord.js"); //Loads and utilizes SlashCommandBuilder and EmbedBuilder FROM discord.js
+
+//this creates the /ping command interface
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("diceroll") //names the command
+		.setDescription("Rolls a dice!") //sets the description
+		.addNumberOption(
+			(
+				option //adds a subcommand that asks for the question in the form of a string, while other varients exist for things like integers
+			) =>
+				option
+					.setName("sides") //names the subcommand
+					.setDescription(
+						"How many sides does this dice have?"
+					) //gives the subcommand a description
+					.setRequired(true) //just makes it mandatory
+		),
+
+	async execute(interaction, bot) {
+		const sides = interaction.options.getNumber("sides");
+		const Boticon =
+			"https://cdn-icons-png.flaticon.com/512/2826/2826187.png";
+
+		if (sides < 1) {
+			return interaction.reply({
+				content: "Number must be more than 1!",
+				ephemeral: true,
+			});
+		}
+
+		min = Math.ceil(1);
+		max = Math.floor(sides);
+		randomNum = Math.floor(
+			Math.random() * (max - min) + min
+		);
+
+		// const embedRoll = new EmbedBuilder()
+		//     .setAuthor({ name: 'Goose', iconURL: `${Boticon}`})
+		//     .setDescription(`Rolling your ${sides} sided dice…`)
+		//     .setColor(0xe3c05f)
+		//     .setTimestamp(Date.now());
+
+		const embedDice = new EmbedBuilder()
+			.setAuthor({ name: "Goose", iconURL: `${Boticon}` })
+			.setDescription(`You rolled a ${randomNum}!`)
+			.setColor(0xe3c05f)
+			.setTimestamp(Date.now())
+			.setFooter({
+				text: ` \n Asked by ${interaction.user.tag} in ${interaction.guild.name}`,
+			});
+
+		await interaction.reply({
+			embeds: [embedDice],
+		});
+	},
+};
+
+//Get the amount of sides
+//Create a temporary loading screen
+//show results
