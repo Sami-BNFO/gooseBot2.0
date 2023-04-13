da85e8c551fb3416400d86e753dd09a111edb597
SamiMAnsari1 <130588384+SamiMAnsari1@users.noreply.github.com>
2023-04-13 16:51:41 +0100

    Update 8ball.js
    autoformat makes me feel things

--- src/commands/fun/8ball.js
+++ src/commands/fun/8ball.js
@@ -1,48 +1,64 @@
 const {
 	SlashCommandBuilder,
 	EmbedBuilder,
-} = require("discord.js"); //imports SlashCommandBuilder and EmbedBuilder from discord.js
+} = require("discord.js"); //Imports SlashCommandBuilder and EmbedBuilder from discord.js
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("8ball")
-		.setDescription("WHAT DOES YOUR DESTINY ENTAIL!!!!!!")
-		.addStringOption(
-			(
-				option //addStringOption adds an option to input a string, addIntOption would add an option to input an int son and so forth
-			) =>
-				option
-					.setName("question")
-					.setDescription(
-						"What would you like to ask the magic 8ball?"
-					)
-					.setRequired(true)
+		.setDescription(
+			"Let goose answer your most philisophical questions"
+		)
+		.addStringOption((option) =>
+			option
+				.setName("question")
+				.setDescription("What is your question?")
+				.setRequired(true)
 		),
 
 	async execute(interaction, bot) {
-		const answer = ["answer a", "answer b", "answer c"];
+		//runs the code
+		const answers = [
+			"I guess?",
+			"no lol",
+			"Obviously, duh",
+			"nope",
+			"thats literally the dumbest question I've ever heard, try something better.",
+			"YES",
+			"NO",
+			"sure, why not",
+			"Did you fail school?? Obviously not.",
+			"What does that even mean?",
+			"No hablo espanol",
+			"Oui",
+			"oo (tämä on suomalainen)",
+			"番号 (これは日本人です)",
+		];
 		const question =
 			interaction.options.getString("question");
-
 		const randomNum = Math.floor(
-			Math.random() * answer.length
+			Math.random() * answers.length
 		);
-
 		const embed = new EmbedBuilder()
 			.setTitle(`${interaction.user.tag} asked:`)
+			.setAuthor({
+				name: "Goose",
+				iconURL:
+					"https://cdn-icons-png.flaticon.com/512/2826/2826187.png",
+			})
+			.setThumbnail(
+				"https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/672-pool-8-ball.svg/768px-672-pool-8-ball.svg.png"
+			)
 			.setDescription(`${question}`)
-			.addFields([
-				{
-					name: `Magic 8ball says:`,
-					value: `${answer[randomNum]}`,
-					inline: true,
-				},
-			])
-			.setColor("Random")
+			.addFields({
+				name: "The magic 8ball says",
+				value: `${answers[randomNum]}`,
+				inline: true,
+			})
+			.setColor(0xe3c05f)
 			.setTimestamp(Date.now())
 			.setFooter({
-				iconURL: `${interaction.user.displayAvatarURL()}`,
-				text: `Asked by ${interaction.user.tag}`,
+				text: ` \n Asked by ${interaction.user.tag} in ${interaction.guild.name}`,
 			});
 
 		await interaction.reply({
