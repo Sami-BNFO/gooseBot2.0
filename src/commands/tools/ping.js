d67e2e92ee3e72ae810dffd805258c4f4729f403
SamiMAnsari1 <130588384+SamiMAnsari1@users.noreply.github.com>
2023-04-13 17:00:30 +0100

    Update ping.js
    found old code

--- src/commands/tools/ping.js
+++ src/commands/tools/ping.js
@@ -1,14 +1,36 @@
-const { SlashCommandBuilder } = require("discord.js");
+const {
+	SlashCommandBuilder,
+	EmbedBuilder,
+} = require("discord.js"); //Loads and utilizes SlashCommandBuilder and EmbedBuilder FROM discord.js
 
+//this creates the /ping command interface
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName("ping")
-		.setDescription("Ping... Pong..."),
+		.setName("ping") //names the command
+		.setDescription("Pings the bot"), //sets the description (bit under the name)
 
 	async execute(interaction, bot) {
+		const embed = new EmbedBuilder()
+			.setTitle("Pings Goose")
+			.setAuthor({
+				name: "Goose",
+				iconURL:
+					"https://cdn-icons-png.flaticon.com/512/2826/2826187.png",
+			})
+			.setThumbnail(
+				"https://images.emojiterra.com/google/noto-emoji/v2.034/512px/1f3d3.png"
+			)
+			.setDescription(
+				`API Latency: ${Math.round(bot.ws.ping)}ms`
+			)
+			.setColor(0xe3c05f)
+			.setTimestamp(Date.now())
+			.setFooter({
+				text: ` \n Asked by ${interaction.user.tag} in ${interaction.guild.name}`,
+			});
+
 		await interaction.reply({
-			contents: `API Latency: ${Math.round(bot.ws.ping)}ms`,
-			ephemeral: true,
+			embeds: [embed],
 		});
 	},
 };
